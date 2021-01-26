63af809c7081524adf27f58d7767f947ce976b8b
Charles Briggs <chucklistmail+site.github.com@gmail.com>
2021-01-26 13:22:38 -0700

    added clean_phone_numbers method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -9,6 +9,17 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def clean_phone_number(phone_number)
+  phone_number = phone_number.tr('^[0-9]', '')
+  if phone_number.length == 10
+    phone_number
+  elsif phone_number.length == 11 && phone_number[0] == '1'
+    phone_number[1..10]
+  else
+    '0000000000'
+  end
+end
+
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -49,13 +60,15 @@ erb_template = ERB.new template_letter
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
-
   zipcode = clean_zipcode(row[:zipcode])
+  phone_number = clean_phone_number(row[:homephone])
 
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
 
   save_thank_you_letter(id, form_letter)
+
+  puts "#{name} #{zipcode} #{phone_number}"
 end
 
